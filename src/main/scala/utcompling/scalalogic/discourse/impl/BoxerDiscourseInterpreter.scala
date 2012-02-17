6c4599c1a593b4b54671a282d4c1bb2568e3b855
Dan Garrette <dhgarrette@gmail.com>
2012-02-17 11:26:51 -0600

    fixed code to extract logical form from boxer output file to account for changes to boxer output

--- src/main/scala/utcompling/scalalogic/discourse/impl/BoxerDiscourseInterpreter.scala
+++ src/main/scala/utcompling/scalalogic/discourse/impl/BoxerDiscourseInterpreter.scala
@@ -20,50 +20,61 @@ import utcompling.scalalogic.discourse.candc.call.impl.CandcImpl
  * [a href='http://svn.ask.it.usyd.edu.au/trac/candc/wiki/boxer']http://svn.ask.it.usyd.edu.au/trac/candc/wiki/boxer[/a]
  */
 class BoxerDiscourseInterpreter[T](
-    private val boxerExpressionInterpreter: BoxerExpressionInterpreter[T] = new Boxer2DrtExpressionInterpreter(),
-    private val candc: Candc = CandcImpl.findBinary(),
-    private val boxer: Boxer = BoxerImpl.findBinary(),
-    private val verbose: Boolean = false)
-    extends DiscourseInterpreter[T] {
+  private val boxerExpressionInterpreter: BoxerExpressionInterpreter[T] = new Boxer2DrtExpressionInterpreter(),
+  private val candc: Candc = CandcImpl.findBinary(),
+  private val boxer: Boxer = BoxerImpl.findBinary(),
+  private val verbose: Boolean = false)
+  extends DiscourseInterpreter[T] {
 
-    override def batchInterpretMultisentence(inputs: List[List[String]], discourseIds: Option[List[String]] = None, question: Boolean = false, verbose: Boolean = false): List[Option[T]] = {
-        val newDiscourseIds = discourseIds.getOrElse((0 until inputs.length).map(_.toString).toList)
-        require(inputs.length == newDiscourseIds.length)
+  override def batchInterpretMultisentence(inputs: List[List[String]], discourseIds: Option[List[String]] = None, question: Boolean = false, verbose: Boolean = false): List[Option[T]] = {
+    val newDiscourseIds = discourseIds.getOrElse((0 until inputs.length).map(_.toString).toList)
+    require(inputs.length == newDiscourseIds.length)
 
-        val candcArgs = Map[String, String](
-            "--candc-printer" -> "boxer")
-        val candcOut = this.candc.batchParseMultisentence(inputs, candcArgs, Some(newDiscourseIds), Some(if (question) "questions" else "boxer"), verbose = verbose)
+    val candcArgs = Map[String, String](
+      "--candc-printer" -> "boxer")
+    val candcOut = this.candc.batchParseMultisentence(inputs, candcArgs, Some(newDiscourseIds), Some(if (question) "questions" else "boxer"), verbose = verbose)
 
-        val boxerArgs = Map[String, String](
-            "--box" -> "false",
-            "--semantics" -> "drs",
-            "--flat" -> "false",
-            "--resolve" -> "true",
-            "--elimeq" -> "true",
-            "--format" -> "prolog",
-            "--instantiate" -> "true")
-        val boxerOut = this.boxer.callBoxer(candcOut, boxerArgs, verbose = verbose)
+    val boxerArgs = Map[String, String](
+      "--box" -> "false",
+      "--semantics" -> "drs",
+      "--flat" -> "false",
+      "--resolve" -> "true",
+      "--elimeq" -> "true",
+      "--format" -> "prolog",
+      "--instantiate" -> "true")
+    val boxerOut = this.boxer.callBoxer(candcOut, boxerArgs, verbose = verbose)
 
-        val drsDict = this.parseBoxerOutput(boxerOut)
-        return newDiscourseIds.map(drsDict.getOrElse(_, None)).toList
+    val drsDict = this.parseBoxerOutput(boxerOut)
+    return newDiscourseIds.map(drsDict.getOrElse(_, None)).toList
+  }
+
+  private def parseBoxerOutput(boxerOut: String): Map[String, Option[T]] = {
+    val drsDict = new MapBuilder[String, Option[T], Map[String, Option[T]]](Map[String, Option[T]]())
+    val singleQuotedRe = """^'(.*)'$""".r
+
+    val lines = boxerOut.split("\n").iterator
+    val IdLineRe = """^id\((\S+),\s*(\d+)\)\.$""".r
+    val SemLineRe = """^sem\((\d+),$""".r
+    for (line <- lines.map(_.trim)) {
+      line match {
+        case IdLineRe(discourseId, drsId) =>
+          lines.next.trim match { case SemLineRe(drsId2) => require(drsId == drsId2, "%s != %s".format(drsId, drsId2)) }
+          require(lines.next.trim.startsWith("[word("))
+          require(lines.next.trim.startsWith("[pos("))
+          require(lines.next.trim.startsWith("[ne("))
+          val drsInput = lines.next.trim.stripSuffix(").")
+
+          val cleanDiscourseId = singleQuotedRe.findFirstMatchIn(discourseId).map(_.group(1)).getOrElse(discourseId)
+          val parsed = this.parseOutputDrs(drsInput, cleanDiscourseId)
+          drsDict += cleanDiscourseId -> Some(this.boxerExpressionInterpreter.interpret(parsed))
+        case _ =>
+      }
     }
+    return drsDict.result
+  }
 
-    private def parseBoxerOutput(boxerOut: String): Map[String, Option[T]] = {
-        val drsDict = new MapBuilder[String, Option[T], Map[String, Option[T]]](Map[String, Option[T]]())
-        val singleQuotedRe = """^'(.*)'$""".r
-        val re = """id\((\S+),(\d+)\)\.\s*sem\((\d+),\s*\[\S*\],\s*\[\S*\],\s*\[\S*\],\s*(\S*)\s*\)\.""".r
-        for (m <- re.findAllIn(boxerOut).matchData) {
-            val List(discourseId, drsId, drsId2, drsInput) = m.subgroups
-            require(drsId == drsId2)
-            val cleanDiscourseId = singleQuotedRe.findFirstMatchIn(discourseId).map(_.group(1)).getOrElse(discourseId)
-            val parsed = this.parseOutputDrs(drsInput, cleanDiscourseId)
-            drsDict += cleanDiscourseId -> Some(this.boxerExpressionInterpreter.interpret(parsed))
-        }
-        return drsDict.result
-    }
-
-    private def parseOutputDrs(drsString: String, discourseId: String): BoxerExpression = {
-        return new BoxerExpressionParser(discourseId).parse(drsString)
-    }
+  private def parseOutputDrs(drsString: String, discourseId: String): BoxerExpression = {
+    return new BoxerExpressionParser(discourseId).parse(drsString)
+  }
 
 }
