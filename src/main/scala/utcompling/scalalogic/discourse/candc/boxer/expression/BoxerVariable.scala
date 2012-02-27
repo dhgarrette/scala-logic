b48d94e4fd2bdf7d2c32cbf11a4bb36db005318c
Dan Garrette <dhgarrette@gmail.com>
2012-02-27 12:42:51 -0600

    fixed bug

--- src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/BoxerVariable.scala
+++ src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/BoxerVariable.scala
@@ -2,9 +2,9 @@ package utcompling.scalalogic.discourse.candc.boxer.expression
 
 import utcompling.scalalogic.top.expression.Variable
 
-case class BoxerVariable(name: String) extends BoxerExpression with Ordered[Variable] {
+case class BoxerVariable(name: String) extends BoxerExpression with Ordered[BoxerVariable] {
 
-    def compare(that: Variable): Int =
+    def compare(that: BoxerVariable): Int =
         this.name.compare(that.name)
 
     def visit[S, R](function: BoxerExpression => S, combinator: List[S] => R, default: R): R = 
