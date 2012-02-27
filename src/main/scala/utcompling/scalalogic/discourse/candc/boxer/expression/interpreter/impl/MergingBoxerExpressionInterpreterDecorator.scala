a2c0296f9524035851961ce9385247af1c14665b
Dan Garrette <dhgarrette@gmail.com>
2012-02-27 12:41:58 -0600

    Added boxer interpreter for removing unnecessary layers of boxes.  fixed other interpreters that weren't recursive.

--- src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/interpreter/impl/MergingBoxerExpressionInterpreterDecorator.scala
+++ src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/interpreter/impl/MergingBoxerExpressionInterpreterDecorator.scala
@@ -5,13 +5,14 @@ import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.BoxerE
 
 class MergingBoxerExpressionInterpreterDecorator extends BoxerExpressionInterpreter[BoxerExpression] {
 
-    override def interpret(e: BoxerExpression) =
-        e match {
-            case BoxerAlfa(_variable, first, second) =>
-                BoxerDrs(first.refs ++ second.refs, first.conds ++ second.conds)
-            case BoxerMerge(_pred, first, second) =>
-                BoxerDrs(first.refs ++ second.refs, first.conds ++ second.conds)
-            case _ => e
-        }
+  override def interpret(e: BoxerExpression) =
+    e match {
+      case BoxerAlfa(_variable, first, second) =>
+        BoxerDrs(first.refs ++ second.refs, first.conds ++ second.conds)
+      case BoxerMerge(_pred, first, second) =>
+        BoxerDrs(first.refs ++ second.refs, first.conds ++ second.conds)
+      case _ =>
+        e.visitConstruct(this.interpret)
+    }
 
 }
--- src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/interpreter/impl/OccurrenceMarkingBoxerExpressionInterpreterDecorator.scala
+++ src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/interpreter/impl/OccurrenceMarkingBoxerExpressionInterpreterDecorator.scala
@@ -5,15 +5,16 @@ import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.BoxerE
 
 class OccurrenceMarkingBoxerExpressionInterpreterDecorator extends BoxerExpressionInterpreter[BoxerExpression] {
 
-    override def interpret(e: BoxerExpression) =
-        e match {
-            case BoxerNamed(discId, indices, variable, name, typ, sense) =>
-                BoxerNamed(discId, indices, variable, "%s_%s_d%s%s".format(name, typ, discId, indices.map("_w" + _).mkString(",")), typ, sense)
-            case BoxerPred(discId, indices, variable, name, pos, sense) =>
-                BoxerPred(discId, indices, variable, "%s_%s_d%s%s".format(name, pos, discId, indices.map("_w" + _).mkString(",")), pos, sense)
-            case BoxerRel(discId, indices, event, variable, name, sense) =>
-                BoxerRel(discId, indices, event, variable, "%s_d%s%s".format(name, discId, indices.map("_w" + _).mkString(",")), sense)
-            case _ => e
-        }
+  override def interpret(e: BoxerExpression) =
+    e match {
+      case BoxerNamed(discId, indices, variable, name, typ, sense) =>
+        BoxerNamed(discId, indices, variable, "%s_%s_d%s%s".format(name, typ, discId, indices.map("_w" + _).mkString(",")), typ, sense)
+      case BoxerPred(discId, indices, variable, name, pos, sense) =>
+        BoxerPred(discId, indices, variable, "%s_%s_d%s%s".format(name, pos, discId, indices.map("_w" + _).mkString(",")), pos, sense)
+      case BoxerRel(discId, indices, event, variable, name, sense) =>
+        BoxerRel(discId, indices, event, variable, "%s_d%s%s".format(name, discId, indices.map("_w" + _).mkString(",")), sense)
+      case _ =>
+        e.visitConstruct(this.interpret)
+    }
 
 }
