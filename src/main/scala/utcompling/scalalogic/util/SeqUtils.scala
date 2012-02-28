68c3263c0cfff615a6da7feb312d6547b9395a8f
Dan Garrette <dhgarrette@gmail.com>
2012-02-28 09:44:22 -0600

    added CollectionUtils

--- src/main/scala/utcompling/scalalogic/util/SeqUtils.scala
+++ src/main/scala/utcompling/scalalogic/util/SeqUtils.scala
@@ -6,7 +6,7 @@ object SeqUtils {
 
   def partitionN[A, Repr <: Traversable[A], That](items: Repr, partitionFunction: A => Int, n: Option[Int] = None)(implicit bf: CanBuildFrom[Repr, A, That]): List[That] = {
     val m = items.groupBy(partitionFunction)
-    (0 until n.getOrElse(m.keys.max + 1)).map { i =>
+    (0 to n.getOrElse(m.keys.max)).map { i =>
       val b = bf(items)
       if (m.contains(i))
         for (x <- m(i))
