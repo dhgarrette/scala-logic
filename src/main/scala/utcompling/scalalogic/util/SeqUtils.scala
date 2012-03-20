e1d2266fb6e3a6a6441d1bba493209bc3d0a1086
Dan Garrette <dhgarrette@gmail.com>
2012-03-20 14:04:34 -0500

    fixed bug in SeqUtils.partitionN

--- src/main/scala/utcompling/scalalogic/util/SeqUtils.scala
+++ src/main/scala/utcompling/scalalogic/util/SeqUtils.scala
@@ -6,13 +6,15 @@ object SeqUtils {
 
   def partitionN[A, Repr <: Traversable[A], That](items: Repr, partitionFunction: A => Int, n: Option[Int] = None)(implicit bf: CanBuildFrom[Repr, A, That]): List[That] = {
     val m = items.groupBy(partitionFunction)
-    (0 to n.getOrElse(m.keys.max)).map { i =>
-      val b = bf(items)
-      if (m.contains(i))
-        for (x <- m(i))
-          b += x
-      b.result
-    }.toList
+    val r =
+      (0 until n.getOrElse(m.keys.max + 1)).map { i =>
+        val b = bf(items)
+        if (m.contains(i))
+          for (x <- m(i))
+            b += x
+        b.result
+      }.toList
+    r
   }
 
 }
