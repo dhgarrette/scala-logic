fe84506cdb5a6c421b7b9d1e404f03c75199396d
Dan Garrette <dhgarrette@gmail.com>
2012-03-20 19:23:29 -0500

    occurrence marker now leaves of discourse id from relations

--- src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/interpreter/impl/OccurrenceMarkingBoxerExpressionInterpreterDecorator.scala
+++ src/main/scala/utcompling/scalalogic/discourse/candc/boxer/expression/interpreter/impl/OccurrenceMarkingBoxerExpressionInterpreterDecorator.scala
@@ -8,13 +8,21 @@ class OccurrenceMarkingBoxerExpressionInterpreterDecorator extends BoxerExpressi
   override def interpret(e: BoxerExpression) =
     e match {
       case BoxerNamed(discId, indices, variable, name, typ, sense) =>
-        BoxerNamed(discId, indices, variable, "%s_%s_d%s%s".format(name, typ, discId, indices.map("_w" + _).mkString(",")), typ, sense)
+        BoxerNamed(discId, indices, variable, "%s_%s%s".format(name, typ, indexString(discId, indices)), typ, sense)
       case BoxerPred(discId, indices, variable, name, pos, sense) =>
-        BoxerPred(discId, indices, variable, "%s_%s_d%s%s".format(name, pos, discId, indices.map("_w" + _).mkString(",")), pos, sense)
+        BoxerPred(discId, indices, variable, "%s_%s%s".format(name, pos, indexString(discId, indices)), pos, sense)
       case BoxerRel(discId, indices, event, variable, name, sense) =>
-        BoxerRel(discId, indices, event, variable, "%s_d%s%s".format(name, discId, indices.map("_w" + _).mkString(",")), sense)
+        BoxerRel(discId, indices, event, variable, "r_%s%s".format(name, indexString(discId, indices)), sense)
       case _ =>
         e.visitConstruct(this.interpret)
     }
+
+  protected def indexString(discId: String, indices: List[BoxerIndex]) = {
+    if (indices.nonEmpty) {
+      "_d%s%s".format(discId, indices.map("_" + _).mkString(""))
+    }
+    else
+      ""
+  }
 
 }
